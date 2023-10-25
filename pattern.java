90d61d1094d3c978b8198bb07eaae498afc04468
Ritesh kumar <115397558+officialriteshkumar@users.noreply.github.com>
2023-10-25 23:54:22 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,13 @@
+public class pattern {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0;i<=n;i++){    
+            System.out.println("****");
+            System.out.println("***");
+            System.out.println("**");
+            System.out.println("*");
+        }
+    } 
+    
+
+ 
