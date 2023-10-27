fe7c6d20ebb4884871d504b89011907b46067b52
Ritesh kumar <115397558+officialriteshkumar@users.noreply.github.com>
2023-10-27 12:14:29 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Solution {
+public static void main(String args[]) {
+Scanner sc = new Scanner(System.in);
+int A = sc.nextInt();
+int B = sc.nextInt();
+int C = sc.nextInt();
+int average = (A + B + C) / 3;
+System.out.println("average is : " + average);
+}
+}
+
+
---
+++ Unary.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Unary {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = (++a);
+        System.out.println(a);
+        System.out.println(b);
+    }
+    
+}
