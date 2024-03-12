fd52848ffd3fb636e8e9e638b250ccdfdf1a503b
Ritesh kumar <115397558+officialriteshkumar@users.noreply.github.com>
2024-03-12 18:24:17 +0530

    Update golu.java

--- golu.java
+++ golu.java
@@ -1,5 +1,4 @@
 public class golu {
-    public class Butterfly {
         public static void butterfly(int n){
             for (int i = 1; i <= n; i++) {
                 for (int j = 1; j <= i; j++) {
