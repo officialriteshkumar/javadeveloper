f800856d29e714625e312814341497538768dce5
Ritesh kumar <115397558+officialriteshkumar@users.noreply.github.com>
2024-04-13 22:46:40 +0530

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,13 +1,30 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 public class Solution {
-public static void main(String args[]) {
-Scanner sc = new Scanner(System.in);
-int A = sc.nextInt();
-int B = sc.nextInt();
-int C = sc.nextInt();
-int average = (A + B + C) / 3;
-System.out.println("average is : " + average);
+    static boolean isAnagram(String A, String B){
+    A=A.toLowerCase();
+    B=B.toLowerCase();
+    boolean f =false;
+    char[]c= A.toCharArray();
+    Arrays.sort(c);
+    char[]d = B.toCharArray();
+    Arrays.sort(d);
+    String a = new String (c);
+    String b= new String (d);
+    if(a.equals(b)){
+        f=true;
+    }
+    return f;
+    }
+    
+    public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
 }
-}
-
 
